f3cb980c5617bdbd848a1a4aea780b72806d791b
Alex Garibay <alex@alexgaribay.com>
2013-10-25 14:43:12 -0500

    First commit. Add gitignore. Add timer class.

---
+++ AGTimer/Model/AGTimer.h
@@ -0,0 +1,13 @@
+//
+//  AGTimer.h
+//  AGTimer
+//
+//  Created by Alex Garibay on 10/25/13.
+//  Copyright (c) 2013 Alex Garibay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTimer : NSObject
+
+@end
---
+++ AGTimer/Model/AGTimer.m
@@ -0,0 +1,13 @@
+//
+//  AGTimer.m
+//  AGTimer
+//
+//  Created by Alex Garibay on 10/25/13.
+//  Copyright (c) 2013 Alex Garibay. All rights reserved.
+//
+
+#import "AGTimer.h"
+
+@implementation AGTimer
+
+@end
